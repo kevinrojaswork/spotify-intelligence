7ba63b576af31045fc18a8a9a7b1937dcb832a51
Kevin Rojas <kevinrojaswork@gmail.com>
2026-06-24 16:26:32 -0600

    Use direct Spotify auth link

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,16 +1,7 @@
+const SPOTIFY_AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=920f42a830964ed6bcb6cdd2205004bc&response_type=code&redirect_uri=https%3A%2F%2Fspotify-intelligence-production.up.railway.app%2Fauth%2Fcallback&scope=playlist-read-private+playlist-read-collaborative+user-library-read+user-read-email+user-top-read+user-read-private";
+
 function Topbar() {
-  const connectSpotify = async () => {
-  const response = await fetch(
-    "https://spotify-intelligence-production.up.railway.app/auth/login"
-  );
-
-  const data = await response.json();
-
-  console.log("Spotify auth data:", data);
-
-  window.location.assign(data.auth_url);
-};
-
   return (
     <header className="topbar">
       <div>
@@ -18,9 +9,9 @@ function Topbar() {
         <h1>Tu centro de inteligencia musical</h1>
       </div>
 
-      <button className="connect-button" onClick={connectSpotify}>
+      <a className="connect-button" href={SPOTIFY_AUTH_URL}>
         Conectar Spotify
-      </button>
+      </a>
     </header>
   );
 }
